85f529d5acacf8c5a030790d8f949680c7e0a82c
jguerreiro <joel.borghi@gmail.com>
2024-04-06 16:38:22 -0300

    leetcode[TwoSum] - First refactor - but still need to improve

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -5,52 +5,55 @@
  */
 const twoSum = (nums, target) => {
 
-  let next;
-  let sum;
-  let result = [];
+  // Current number auxiliar variable.
+  let currentValue = 0;
+  // Sum of the current + next.
+  let sum = 0;
 
+  // Indexes of number' that the sum equals targets.
+  let firstIndex = 0;
+  let secondIndex = 0;
+
+  // Edge cases
+  // Single number ex: [3]; index = 0
+  // 2 numbers [3,3] (6)
+
+  let counter = 0;
   for (i = 0; i < nums.length; i++) {
+    currentValue = nums[counter];
 
-    if (nums.length < 3) {
-      result = [0, 1];
-      break;
-    } else if (nums.length >= 3) {
-      next = nums[i + 1];
-      sum = nums[i] + next;
-
+    for (j = 1; j < nums.length; j++) {
+      sum = currentValue + nums[j];
       if (sum === target) {
-        result.push(nums.indexOf(nums[i]));
-        result.push(nums.indexOf(nums[i + 1]));
+        console.log('entrou')
+        firstIndex = i;
+        secondIndex = j;
 
-      } else if (sum < target) {
-        sum = 0;
-        next = nums[i + 2];
-        sum = nums[i] + next;
-
-        if (sum < target) {
-          sum = 0;
-          next = nums[i + 2];
-          sum = nums[i + 1] + next;
-
-          if (sum === target) {
-            result.push(1);
-            result.push(2);
-            console.log(result);
-            break;
-          }
-        } else {
-          if (sum === target) {
-            result.push(0);
-            result.push(2);
-            console.log(result);
-            break;
-          }
-        }
+        break;
       }
+      counter++;
     }
   }
-  console.log(result)
-  return result;
+
+  let result = [];
+
+  // Edge cases
+  if (nums.length === 1) {
+    result = [firstIndex];
+  } else if (nums.length === 2) {
+    result = [0, 1];
+  } else if (nums[0] + nums[1] === target) {
+    result = [0, 1];
+    // default
+  } else {
+    result = [firstIndex, secondIndex];
+
+    if (result[0] > result[1]) {
+      result = result.reverse();
+    }
+  }
+  console.log(result);
 }
 
-twoSum([3,2,4], 6);
+// Need to fix this edge case:
+twoSum([2,5,5,11], 10);
